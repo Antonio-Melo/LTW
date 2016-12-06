13ee67ac71517d9bd18cec2c9f25c114cac24c73
Antonio-Melo <happyd3nt@gmail.com>
2016-12-06 16:02:13 +0000

    Ex2 done

---
+++ Ficha3 PHP/Ex2/sum2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sum Two Numbers</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <?php
+        $num1 = $_GET['num1'];
+        $num2 = $_GET['num2'];
+        echo "<p>" . ($num1+$num2) . "</p>";
+     ?>
+     <a href="home.html">Back</a>
+  </body>
+</html>
